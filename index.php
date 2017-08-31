498e527fe29a357a0d783f3ccf84ca1b3e45e274
WildFilming <31490780+WildFilming@users.noreply.github.com>
2017-08-31 17:42:21 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-<?php die("a:); ?>
+<?php die("a"); ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Page Title</title>
+<title>Page Title2</title>
 </head>
 <body>
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+<h1>This is a Hea2ding</h1>
+<p>This is a parag2raph.</p>
 
 </body>
 </html>
